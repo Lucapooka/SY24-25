a740a601a4276c6e9821daab738627426874974c
Luca <Lvfru131@learner.dvusd.org>
2024-12-05 14:10:21 -0700

    pixels

--- pixel paint/pixel paint/MainWindow.xaml.vb
+++ pixel paint/pixel paint/MainWindow.xaml.vb
@@ -1,3 +1,5 @@
 ﻿Class MainWindow
-
+    Private Sub fill_1_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles fill_1.MouseDown, fill_2.MouseDown, fill_3.MouseDown, fill_4.MouseDown, fill_5.MouseDown, fill_6.MouseDown, fill_7.MouseDown, fill_8.MouseDown
+        brush_1.Fill = sender.fill
+    End Sub
 End Class
